ee1e75a8922f12ea019483396dc08fd84ca1cd00
Stefan Holmgren <stehol@gmail.com>
2024-04-07 15:46:05 +0200

    Update transition properties in Letters.module.scss and SecretWord.module.scss

--- src/pages/play/components/SecretWord.tsx
+++ src/pages/play/components/SecretWord.tsx
@@ -1,53 +1,49 @@
 import style from "./SecretWord.module.scss";
-import { Accessor, For, Setter, createEffect, createSignal, onCleanup, onMount } from "solid-js";
+import { For, createEffect, createSignal, onCleanup, onMount } from "solid-js";
 
 type SecretWordProps = {
   word: string;
   guessedLetters: string[];
 };
 
-type SecretLetter = {
-  letter: string;
-  guessed: Accessor<boolean>;
-  setGuessed: Setter<boolean>;
-};
-
-const createSecretLetter = (letter: string): SecretLetter => {
+const createSecretLetter = (letter: string) => {
   const [guessed, setGuessed] = createSignal(false);
-  return { letter, guessed, setGuessed };
+  return {
+    letter,
+    guessed,
+    setGuessed,
+  };
 };
 
 export default function SecretWord(props: SecretWordProps) {
   const [secretLetters] = createSignal(props.word.toUpperCase().split("").map(createSecretLetter));
-  const [breaks, setBreaks] = createSignal<number[]>([]);
 
+  // Update guessed state based on guessedLetters prop
   createEffect(() => {
-    for (const guessedLetter of props.guessedLetters) {
-      const secretLetter = secretLetters().find((letter) => letter.letter === guessedLetter);
-      if (secretLetter) {
+    secretLetters().forEach((secretLetter) => {
+      if (props.guessedLetters.includes(secretLetter.letter)) {
         secretLetter.setGuessed(true);
       }
-    }
+    });
   });
 
+  // Update line breaks based on layout changes
   let letterRefs: HTMLSpanElement[] = [];
+  const [breaks, setBreaks] = createSignal<number[]>([]);
+
   const updateBreaks = () => {
     const newBreaks: number[] = [];
     letterRefs.forEach((el, index) => {
-      const previousLetter = el.previousElementSibling;
-      if (!previousLetter || !(previousLetter instanceof HTMLSpanElement)) {
-        return;
-      }
-      if (el.offsetTop != previousLetter.offsetTop) {
+      const previousLetter = letterRefs[index - 1];
+      if (previousLetter && el.offsetTop != previousLetter.offsetTop) {
         newBreaks.push(index - 1);
       }
     });
     setBreaks(newBreaks);
   };
 
-  createEffect(updateBreaks);
-
   onMount(() => {
+    updateBreaks();
     window.addEventListener("resize", updateBreaks);
   });
 
