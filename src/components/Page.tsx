4bbaa634791d498bc76714ef3fdb774e5ebaf058
Stefan Holmgren <stehol@gmail.com>
2024-04-07 14:44:36 +0200

    Update styles and components for game play

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,8 @@
-import { JSX } from "solid-js";
+import { JSX, onCleanup, onMount } from "solid-js";
 import Backdrop from "./Backdrop";
 import style from "./Page.module.scss";
 import PageHeader from "./PageHeader";
+import { useNavigate } from "@solidjs/router";
 
 type PageProps = {
   title: string;
@@ -10,6 +11,23 @@ type PageProps = {
 };
 
 export default function Page(props: PageProps) {
+  const navigate = useNavigate();
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      e.preventDefault();
+      navigate(props.backHref);
+    }
+  };
+
+  onMount(() => {
+    document.addEventListener("keydown", onKeyDown);
+  });
+
+  onCleanup(() => {
+    document.removeEventListener("keydown", onKeyDown);
+  });
+
   return (
     <Backdrop class={style.page}>
       <PageHeader title={props.title} backHref={props.backHref} />
--- src/pages/pick-category/PickCategory.tsx
+++ src/pages/pick-category/PickCategory.tsx
@@ -2,15 +2,20 @@ import style from "./PickCategory.module.scss";
 import Category from "./components/Category";
 
 import data from "../../assets/data.json";
-import { For } from "solid-js";
+import { For, onMount } from "solid-js";
 import Page from "@/components/Page";
 
 export default function PickCategory() {
   const categories = Object.keys(data.categories);
+  let categoriesRef: HTMLUListElement | undefined;
+
+  onMount(() => {
+    console.log("categoriesRef", categoriesRef);
+  });
 
   return (
     <Page title="Pick a Category" backHref="/">
-      <ul class={style.categories}>
+      <ul ref={categoriesRef} class={style.categories}>
         <For each={categories}>
           {(category) => (
             <li>
